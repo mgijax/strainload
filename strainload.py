4c1cec51df82e6813ce25ae785bf3068a660214b
MGI Admin <mgiadmin@jax.org>
2013-10-21 15:31:23 +0000

    strainload-5-1-4-5

--- strainload.py
+++ strainload.py
@@ -495,10 +495,11 @@ def processFile():
 
         # MGI Accession ID for the strain
 
-        accFile.write('%d|%s%d|%s|%s|1|%d|%d|0|1|%s|%s|%s|%s\n' \
-          % (accKey, mgiPrefix, mgiKey, mgiPrefix, mgiKey, strainKey, mgiTypeKey, 
-	     createdByKey, createdByKey, cdate, cdate))
-        accKey = accKey + 1
+	if isStandard == 1:
+        	accFile.write('%d|%s%d|%s|%s|1|%d|%d|0|1|%s|%s|%s|%s\n' \
+          	% (accKey, mgiPrefix, mgiKey, mgiPrefix, mgiKey, strainKey, mgiTypeKey, 
+	     	   createdByKey, createdByKey, cdate, cdate))
+        	accKey = accKey + 1
 
         # external accession id
         # % (accKey, id, '', id, externalLDB, strainKey, externalTypeKey, 
